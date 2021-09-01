789d4ecdb77d955df408b4689ae0af21e9f2953d
Alejahndro <alejahndro02@gmail.com>
2021-09-01 15:32:53 -0500

    Se importo y se ejecuto la conexion de la base de datos desde el archivo db.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+//Se impora la libreria de doenv para poder uilizar las variables guardadas en el archivo env
 require('dotenv').config(); //Esto es qeuivalente a const dotenv =required('dotenv); dotenv.config
 
 const express = require('express');
-const { appConfig } = require('./config');
+//Se importa la db
+const connectDb = require('./db/db');
+
+//Se importa el achivo config donde se encuentran las configuraciones de las variables de entorno
+const { appConfig, dbConfig } = require('./config');
 const app = express();
 
+connectDb(dbConfig);
 app.listen(appConfig.port, () => console.log(`Escuchando por el puerto ${appConfig.port}`))
