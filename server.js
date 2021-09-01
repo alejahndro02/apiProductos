9330a346b33a29ee2c2952b914d1727456ee8e8c
Alejahndro <alejahndro02@gmail.com>
2021-09-01 14:54:15 -0500

    Se importo la dependecia dotenv y el archivo de configuracion

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require('dotenv').config(); //Esto es qeuivalente a const dotenv =required('dotenv); dotenv.config
+
 const express = require('express');
+const { appConfig } = require('./config');
 const app = express();
-const port = 8080;
-app.listen(port, () => console.log(`Escuchando por el puerto ${port}`))
+
+app.listen(appConfig.port, () => console.log(`Escuchando por el puerto ${appConfig.port}`))
