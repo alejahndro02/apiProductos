c1f33e0f34f99b94a6697881dc2e803a47147905
Alejahndro <alejahndro02@gmail.com>
2021-09-01 16:42:24 -0500

    Se establece como una funcion asincrona mogoose.connect

--- db/db.js
+++ db/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connection.on('open', () => console.log('Base de datod conectada'))
 async function connectDb({ host, port, dbName }) {
     const uri = `mongodb://${ host }:${ port }/${ dbName }`
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 module.exports = connectDb;
