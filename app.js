adc02b6ad87676d8b8c0e1b2d924a3707ccdf71f
Alejahndro <alejahndro02@gmail.com>
2021-09-01 19:51:04 -0500

    Se usa express.json y express.urlencoded para parsear la data

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const productRoutes = require('./routes/product')
 
 const app = express();
 
-app.use('/v1/', productRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })),
+
+
+    app.use('/v1/', productRoutes);
 
 module.exports = app;
