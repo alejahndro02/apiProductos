5fbe0f679f059f93ecf3259cbd7759fc14aec930
Alejahndro <alejahndro02@gmail.com>
2021-09-02 23:50:08 -0500

    Se delcaro un metodo SetImgUrl donde se configura elnombre del archivo con cual sera almecando

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
+const { appConfig } = require('../config');
 const Schema = mongoose.Schema;
 //Se crea una collection 
 const ProductSchema = Schema({
     name: String,
     size: Number,
+    imgUrl: String,
     unitaryPrice: Number,
     description: String
 }, {
     //Se registra la hora y si hubo alguna modificacion 
     timestamps: true
 });
+// se crea un metodo 
+ProductSchema.methods.setImgUrl = function setImgUrl(fileName) {
+    const { host, port } = appConfig;
+    this.imgUrl = `${host}:${ port}/public/${fileName}`
+}
+
 module.exports = mongoose.model('Products', ProductSchema);
