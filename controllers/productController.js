a79884d7111b022f2cd600a181cad04ad4853c70
Alejahndro <alejahndro02@gmail.com>
2021-09-02 23:53:16 -0500

    Se hace una instacia para poder guardar la ruta que viene del modelo

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,9 +3,13 @@ const Product = require('../models/Product');
 async function addProduct(req, res) {
     try {
         const { name, size, unitaryPrice, description } = req.body;
-
-        //Se hace una instacia del modelo
+        //Se hace una instacia del modelo para poder guardar la ruta 
         const product = Product({ name, size, unitaryPrice, description });
+
+        if (req.file) {
+            const { fileName } = req.file;
+            Product.setImgUrl(fileName);
+        }
 
         //se guardan los datos en la base con el metodo save()
         const productStored = await product.save();
